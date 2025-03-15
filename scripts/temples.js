aa457f2cbd8cca021dddf29844c9dbfdb592c43d
Trevor <trevormngwenya@gmail.com>
2025-03-15 17:22:49 +0200

    Edit mobile nav menu

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,13 @@
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.navigation');
+const hambuttom = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambuttom.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambuttom.classList.toggle('show');
+});
+
 const link = 'https://assets.churchofjesuschrist.org/59/a8/59a867c4052795ee6ac95c839229b3ec41b0c656/mexico_city_temple_exterior.jpeg';
 // const link = '../images/mexico-city-temple.png';
 
@@ -18,12 +28,10 @@ for (i = 0; i < 16; i++) {
     temples.append(getTemple(link));
 }
 
-// Store the selected elements that we are going to use. 
-const mainnav = document.querySelector('.navigation');
-const hambuttom = document.querySelector('#menu');
+// Set dates
+const currentYear = document.querySelector('#year');
+const lastModified = document.querySelector('#lastModified');
 
-// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
-hambuttom.addEventListener('click', () => {
-    mainnav.classList.toggle('show');
-    hambuttom.classList.toggle('show');
-});
+currentYear.innerText = new Date().getFullYear();
+lastModified.innerText = `Last Modification: ${document.lastModified}`;
+
